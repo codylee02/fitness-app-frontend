5966bc82b530c5dee7a6ef3dfeedf3500589342f
Cody Lee <codylee02@gmail.com>
2024-10-16 21:46:51 -0600

    rm storybook and get local dev working

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'fitness-app-frontend',
+  appId: 'ionic.fafe',
+  appName: 'fafe',
   webDir: 'dist'
 };
 
